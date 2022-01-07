ce5782b77772a92da4ee279fc93890554ffb3e3e
MT Nguyen <nmtuan2001@gmail.com>
2022-01-07 14:38:39 +0800

    Minor change to main.py

--- main.py
+++ main.py
@@ -23,7 +23,6 @@ LOCATION, RESTAURANT, CAPACITY, TIME, CONFIRMATION = range(5)
 reply_keyboard = [['Confirm', 'Restart']]
 markup = ReplyKeyboardMarkup(reply_keyboard, resize_keyboard=True, one_time_keyboard=True)
 bot = telegram.Bot(token=TOKEN)
-#chat_id = 'YOURTELEGRAMCHANNEL'
 gmaps = GoogleMaps(GMAPSAPI)
 
 PORT = int(os.environ.get('PORT', 5000))
@@ -118,7 +117,7 @@ def confirmation(update, context):
 
 def cancel(update, context):
     user = update.message.from_user
-    logger.info("User %s canceled the conversation.", user.first_name)
+    logger.info("User %s cancelled the conversation.", user.first_name)
     update.message.reply_text('Bye! Hope to see you again next time.',
                               reply_markup=ReplyKeyboardRemove())
 
@@ -177,7 +176,6 @@ def main():
     # start_polling() is non-blocking and will stop the bot gracefully.
     #updater.idle()
     updater.start_polling()
-
 
 
 if __name__ == '__main__':
